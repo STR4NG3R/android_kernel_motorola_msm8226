c4e35b76a343dbc13ed4ec64db969d463d7b2be5
Rabih Salamey <somcom3@gmail.com>
2015-07-24 04:48:17 -0400

    new camera code

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -340,6 +340,7 @@ struct msm_vfe_axi_shared_data {
 	struct msm_vfe_src_info src_info[VFE_SRC_MAX];
 	uint16_t stream_handle_cnt;
 	unsigned long event_mask;
+	uint32_t burst_len;
 };
 
 struct msm_vfe_stats_hardware_info {
@@ -381,6 +382,11 @@ struct msm_vfe_stats_shared_data {
 	atomic_t stats_comp_mask;
 	uint16_t stream_handle_cnt;
 	atomic_t stats_update;
+<<<<<<< HEAD
+=======
+	uint32_t stats_mask;
+	uint32_t stats_burst_len;
+>>>>>>> c4916de... Revert "Revert "msm-camera: move target checks to userspace""
 };
 
 struct msm_vfe_tasklet_queue_cmd {
@@ -438,8 +444,7 @@ struct vfe_device {
 	struct list_head tasklet_q;
 	struct tasklet_struct vfe_tasklet;
 	struct msm_vfe_tasklet_queue_cmd
-		tasklet_queue_cmd[MSM_VFE_TASKLETQ_SIZE];
-
+	tasklet_queue_cmd[MSM_VFE_TASKLETQ_SIZE];
 	uint32_t soc_hw_version;
 	uint32_t vfe_hw_version;
 	struct msm_vfe_hardware_info *hw_info;
@@ -454,6 +459,7 @@ struct vfe_device {
 	void __iomem *p_avtimer_msw;
 	void __iomem *p_avtimer_lsw;
 	uint8_t ignore_error;
+	uint32_t vfe_ub_size;
 };
 
 #endif
